f15750f5bbfaf8ebfe09d0a2b1d155d5808e5eae
Anna Axmatova <ax.annushka@yandex.ru>
2024-10-22 04:04:00 +0300

    Subscrib

---
+++ src/components/Landing/Lan_Subscribtion/Subscrib.jsx
@@ -0,0 +1,16 @@
+import './Subscrib.css'
+import Button from "../../UI/Button"
+
+export default function Subscrib() {
+	return (
+		<div className="subscribtion-container">
+			<div className="sub-info-left text-title-m">
+				Подпишитесь на наши обновления, чтобы не пропустить интересные новости!
+			</div>
+			<div className="sub-info-right">
+				
+				<Button />
+			</div>
+		</div>
+	)
+}
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,6 +2,7 @@ import Info from "../components/Landing/Lan_Info/Info";
 import About from "../components/Landing/Lan_About/About";
 import Sections from "../components/Landing/Lan_Category/Category";
 import Posts from "../components/Landing/Lan_Posts/Posts";
+import Subscrib from "../components/Landing/Lan_Subscribtion/Subscrib";
 import Navigation from "../components/Navigation/Navbar";
 import './Index.css'
 
@@ -12,6 +13,7 @@ export default function Index() {
 			<About />
 			<Sections />
 			<Posts />
+			<Subscrib />
 			<Navigation />
 		</div>
 	);
