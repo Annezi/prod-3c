77df4ce081b8598066eb298b08bfdf8e82c61d21
Anna Axmatova <ax.annushka@yandex.ru>
2024-11-21 18:54:41 +0300

    Bugs Fixing

--- src/components/Articles/Art_Banner/Banner.jsx
+++ src/components/Articles/Art_Banner/Banner.jsx
@@ -12,36 +12,35 @@ export default function Banner() {
 	];
 
 	return (
-		<Helmet>
-			<meta property="og:type" content="article" />
-			<meta property="og:image" content="/public/Articles/articles_cover.jpg"/>
-			<meta property="og:image:width" content="1200"/>
-			<meta property="og:image:height" content="630"/>
-			<meta property="og:title" content="Первое апреля в Японии"/>
-			<meta property="og:description" content="Какие розыгрыши устраивают японцы, и где можно купить ведерко куриных косточек"/>
-
-			<div className="banner-container">
-				<div className="ban-infoSub">
-					<div className="ban-breadcrumbs">
-						<Breadcrumbs items={breadcrumbItems} />
-					</div>
-					<div className="ban-readTime">
-						<div className="readTime-title text-paragraph-m">Время чтения :</div>
-						<div className="readTime-time text-paragraph-m">~5 минут</div>
-					</div>
+		<div className="banner-container">
+			<Helmet>
+				<meta property="og:type" content="article" />
+				<meta property="og:image" content="/public/Articles/articles_cover.jpg" />
+				<meta property="og:image:width" content="1200" />
+				<meta property="og:image:height" content="630" />
+				<meta property="og:title" content="Первое апреля в Японии" />
+				<meta property="og:description" content="Какие розыгрыши устраивают японцы, и где можно купить ведерко куриных косточек" />
+			</Helmet>
+			<div className="ban-infoSub">
+				<div className="ban-breadcrumbs">
+					<Breadcrumbs items={breadcrumbItems} />
 				</div>
-				<div className="ban-infoMain">
-					<div className="infoMain-ArticleInfo">
-						<div className="ArticleInfo-tags">
-							<Tag text="Культура" />
-							<Tag text="Праздники" />
-						</div>
-						<div className="ArticleInfo-title text-title-l">Первое апреля в Японии</div>
-						<div className="ArticleInfo-description text-paragraph-m">Какие розыгрыши устраивают японцы, и где можно купить ведерко куриных косточек</div>
-					</div>
-					<div className="infoMain-publushDate text-paragraph-s">22 октября 2024</div>
+				<div className="ban-readTime">
+					<div className="readTime-title text-paragraph-m">Время чтения :</div>
+					<div className="readTime-time text-paragraph-m">~5 минут</div>
 				</div>
 			</div>
-		</Helmet>
+			<div className="ban-infoMain">
+				<div className="infoMain-ArticleInfo">
+					<div className="ArticleInfo-tags">
+						<Tag text="Культура" />
+						<Tag text="Праздники" />
+					</div>
+					<div className="ArticleInfo-title text-title-l">Первое апреля в Японии</div>
+					<div className="ArticleInfo-description text-paragraph-m">Какие розыгрыши устраивают японцы, и где можно купить ведерко куриных косточек</div>
+				</div>
+				<div className="infoMain-publushDate text-paragraph-s">22 октября 2024</div>
+			</div>
+		</div>
 	)
 }
