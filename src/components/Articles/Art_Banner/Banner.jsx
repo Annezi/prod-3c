6d2735b3609f10834d4c51f9b16bbaeb7ea38b42
Anna Axmatova <ax.annushka@yandex.ru>
2024-11-21 19:21:21 +0300

    Meta Tags on Index

--- src/components/Articles/Art_Banner/Banner.jsx
+++ src/components/Articles/Art_Banner/Banner.jsx
@@ -2,25 +2,15 @@ import './Banner.css'
 import Tag from "../../UI/Tags/Tag"
 import Breadcrumbs from "../../UI/Breadcrumbs/Breadcrumbs"
 
-import { Helmet } from "react-helmet-async";
-
 export default function Banner() {
 	const breadcrumbItems = [
-		{ label: 'Статьи', url: '/Articles' },
-		{ label: 'Культура', url: '/Culture' },
-		{ label: 'Праздники', url: '/Culture/Holidays' }
+		{ label: 'Статьи', url: 'Articles' },
+		{ label: 'Культура', url: 'Culture' },
+		{ label: 'Праздники', url: 'Culture/Holidays' }
 	];
 
 	return (
 		<div className="banner-container">
-			<Helmet>
-				<meta property="og:type" content="article" />
-				<meta property="og:image" content="/public/Articles/articles_cover.jpg" />
-				<meta property="og:image:width" content="1200" />
-				<meta property="og:image:height" content="630" />
-				<meta property="og:title" content="Первое апреля в Японии" />
-				<meta property="og:description" content="Какие розыгрыши устраивают японцы, и где можно купить ведерко куриных косточек" />
-			</Helmet>
 			<div className="ban-infoSub">
 				<div className="ban-breadcrumbs">
 					<Breadcrumbs items={breadcrumbItems} />
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,9 +6,20 @@ import Subscrib from "../components/Landing/Lan_Subscribtion/Subscrib";
 import HeaderPromo from "../components/Navigation/NavbarPromo/NavbarPromo";
 import Footer from "../components/Navigation/Footer/Footer";
 
+import { Helmet } from "react-helmet-async";
+
 export default function Index() {
 	return (
 		<div className="LandingBox">
+			<Helmet>
+				<meta property="og:url" content="https://annezi.github.io/prod-3c/#/articles" />
+				<meta property="og:type" content="article" />
+				<meta property="og:image" content="/public/Articles/articles_cover.jpg" />
+				<meta property="og:image:width" content="1200" />
+				<meta property="og:image:height" content="630" />
+				<meta property="og:title" content="Первое апреля в Японии" />
+				<meta property="og:description" content="Какие розыгрыши устраивают японцы, и где можно купить ведерко куриных косточек" />
+			</Helmet>
 			<HeaderPromo />
 			<Info />
 			<About />
