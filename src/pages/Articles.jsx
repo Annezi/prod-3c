36f7f6c2496a6d5137dbd3188cc14f7f0ae06542
Anna Axmatova <ax.annushka@yandex.ru>
2024-11-21 18:57:56 +0300

    MetaTags

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -5,9 +5,19 @@ import ShareButtons from "../components/UI/SocialMadiaLinks/SocialMadiaLinks"
 import ReadMore from "../components/Articles/Art_ReadMore/ReadMore";
 import Footer from "../components/Navigation/Footer/Footer";
 
+import { Helmet } from "react-helmet-async";
+
 export default function Articles() {
 	return (
 		<div className="ContainerBox">
+			<Helmet>
+				<meta property="og:type" content="article" />
+				<meta property="og:image" content="/public/Articles/articles_cover.jpg" />
+				<meta property="og:image:width" content="1200" />
+				<meta property="og:image:height" content="630" />
+				<meta property="og:title" content="Первое апреля в Японии" />
+				<meta property="og:description" content="Какие розыгрыши устраивают японцы, и где можно купить ведерко куриных косточек" />
+			</Helmet>
 			<Header />
 			<Banner />
 			<Essay />
